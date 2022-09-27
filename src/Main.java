c16eee03333b99a00fffb5c9356dba5c06162997
Dimon-Ts <tselishev.dimon@yandex.ru>
2022-09-27 23:17:47 +0300

    Учебный тренинг вместе с Ильёй Сазоновым.

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] weights = {90, 91, 93, 92, 85, 87, 88, 89, 0, 0, 0, 0};
+        weights[0] = 90;
+        int januaryWeight = weights[0];
+        System.out.println(januaryWeight);
+        System.out.println(weights[0]);
+        System.out.println(weights[4]);
+        int january = 0;
+        System.out.println(weights[january]);
+        for (int i = 0; i < weights.length; i++) {
+            System.out.println(weights[i]);
+        }
+        int [] arr = new int[]{1 , 10, 55,  67, 88, 90,};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+
+        }
+
+
+        }
+
+
+
+    }
